90fb5ee3c3e0f125cd48ae761ae7b5a045b36d69
Daniel Kreuzhofer <daniel@kreuzhofer.de>
2016-04-27 17:18:22 +0200

    fix order of commands

--- iis/install.ps1
+++ iis/install.ps1
@@ -1,11 +1,12 @@
 Install-Module -Name 'Carbon' -Force
 Import-Module 'Carbon'
 
+# Enable Web Server Feature
+Start-Process -FilePath "C:\Windows\System32\Dism.exe" -ArgumentList "/online /enable-feature /FeatureName:IIS-WebServerRole" -NoNewWindow -Wait
+
 # Create local user account
 $secpasswd = ConvertTo-SecureString “Passw0rd!” -AsPlainText -Force
 $mycreds = New-Object System.Management.Automation.PSCredential (“localiisuser”, $secpasswd)
 Install-User -Credential $mycreds -UserCannotChangePassword -PasswordExpires false
 Add-GroupMember -Name 'IIS_IUSRS' -Member 'localiisuser'
 
-
-Start-Process -FilePath "C:\Windows\System32\Dism.exe" -ArgumentList "/online /enable-feature /FeatureName:IIS-WebServer"

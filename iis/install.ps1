38b8160c175efece54189f80cf208db55f97df29
Daniel Kreuzhofer <daniel@kreuzhofer.de>
2016-04-27 21:58:56 +0200

    adding powershell extensions

--- iis/install.ps1
+++ iis/install.ps1
@@ -1,8 +1,13 @@
-Install-Module -Name 'Carbon' -Force
-Import-Module 'Carbon'
+# Download Powershell Extensions
+Invoke-RestMethod -Uri "http://dkreuzh.blob.core.windows.net/deployment/PackageManagement_x64.msi" -OutFile "PackageManagement_x64.msi"
+Start-Process -FilePath "C:\Windows\System32\msiexec.exe" -ArgumentList "/i PackageManagement_x64.msi /quiet" -NoNewWindow -Wait
 
 # Enable Web Server Feature
 Start-Process -FilePath "C:\Windows\System32\Dism.exe" -ArgumentList "/online /enable-feature /FeatureName:IIS-WebServerRole" -NoNewWindow -Wait
+
+# Install Carbon
+Install-Module -Name 'Carbon' -Force
+Import-Module 'Carbon'
 
 # Create local user account
 $secpasswd = ConvertTo-SecureString “Passw0rd!” -AsPlainText -Force

4bc9e38ec67eaf1f93f6226159aab313b4ae7801
Daniel Kreuzhofer <daniel@kreuzhofer.de>
2016-04-27 17:03:50 +0200

    add user

--- iis/install.ps1
+++ iis/install.ps1
@@ -1,1 +1,11 @@
+Install-Module -Name 'Carbon' -Force
+Import-Module 'Carbon'
+
+# Create local user account
+$secpasswd = ConvertTo-SecureString “Passw0rd!” -AsPlainText -Force
+$mycreds = New-Object System.Management.Automation.PSCredential (“localiisuser”, $secpasswd)
+Install-User -Credential $mycreds -UserCannotChangePassword -PasswordExpires false
+Add-GroupMember -Name 'IIS_IUSRS' -Member 'localiisuser'
+
+
 Start-Process -FilePath "C:\Windows\System32\Dism.exe" -ArgumentList "/online /enable-feature /FeatureName:IIS-WebServer"
